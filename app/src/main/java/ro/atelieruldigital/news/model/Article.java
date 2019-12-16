0154cc282499be801fae7ee4779f101d1ac5a177
Hututui Andrei <ahututui@gmail.com>
2019-12-16 15:15:08 +0200

    First commit on my branch

---
+++ app/src/main/java/ro/atelieruldigital/news/model/Article.java
@@ -0,0 +1,6 @@
+package ro.atelieruldigital.news.model;
+
+public class Article {
+
+    private String name;
+}
---
+++ app/src/main/java/ro/atelieruldigital/news/model/NewsListResponse.java
@@ -0,0 +1,4 @@
+package ro.atelieruldigital.news.model;
+
+public class NewsListResponse {
+}
